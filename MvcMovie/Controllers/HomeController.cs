0f0cfc5b6d46d6e58df1767c4b590c99c8498f43
zaels <luysl2002@gmail.com>
2022-09-03 12:07:29 -0300

    initial commit

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+
+}
